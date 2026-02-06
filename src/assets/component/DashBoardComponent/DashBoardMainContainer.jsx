4b204323b9307d78666fc52c61b1816280f91d72
thibault-de-boutray <thibault.de-boutray@etu.utc.fr>
2026-02-06 18:43:49 +0100

    debut main dashboard

---
+++ src/assets/component/DashBoardComponent/DashBoardMainContainer.jsx
@@ -0,0 +1,19 @@
+
+export const DashBoardMainContainer = () => {
+    return (
+        <div className="dashboard-main">
+            <div className="dashboard-left" name="content-gauche">
+
+            </div>
+            <div name="content-droite" className="dashboard-right">
+                <div className="dashboard-right-top" name="content-right-top">
+
+                </div>
+                <div className="dashboard-right-bottom" name="content-right-bottom">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,14 +1,16 @@
 import { DashBoardTopContainer } from "../assets/component/DashBoardComponent/DashBoadTopContainer"
+import { DashBoardMainContainer } from "../assets/component/DashBoardComponent/DashBoardMainContainer"
 import { useUserContext } from "../Context/UserContext"
 
 export const DashboardPage = () => {
     const { user } = useUserContext()
     return (
-        <div className="flex flex-col mt-10 mx-auto bg-black w-[80vw] space-y-6">
+        <div className="flex flex-col mt-10 mx-auto w-[80vw] space-y-12">
             <h1 className="block text-4xl">
                 Welcome {user.identifiant}
             </h1>
             <DashBoardTopContainer />
+            <DashBoardMainContainer />
         </div>
     )
 }
