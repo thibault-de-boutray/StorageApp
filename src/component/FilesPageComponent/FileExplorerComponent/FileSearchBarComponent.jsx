a8e2118150ebb71286afe637ab9eee41b20dcbaa
thibault-de-boutray <thibault.de-boutray@etu.utc.fr>
2026-02-07 21:22:36 +0100

    AJOUT USESTATE

--- src/component/FilesPageComponent/FileExplorerComponent/FileSearchBarComponent.jsx
+++ src/component/FilesPageComponent/FileExplorerComponent/FileSearchBarComponent.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
 import { FaMagnifyingGlass } from "react-icons/fa6"
 export const FileSearchBarComponent = () => {
+    const [research, setResearch] = useState("")
+    const handleChange = (e) => {
+        setResearch(e.target.value)
+    }
     return (
         <div className="relative w-[60%] ">
-            <input type='text' placeholder='Search Files...' className=' w-[100%] pl-10 text-white text-0.7xl border-white/20 border-1 p-2 bg-slate-800 rounded-xl' />
+            <input type='text' placeholder='Search Files...' value={research} onChange={handleChange} className=' w-[100%] pl-10 text-white text-0.7xl border-white/20 border-1 p-2 bg-slate-800 rounded-xl' />
             <FaMagnifyingGlass className="absolute top-3.5 left-3" />
         </div>
     )
